bfcabbb9c995726f79784d7d624723068d2bc3a7
yassin <makrai.yassin@gmail.com>
2020-10-27 10:08:54 +0100

    changement id canvas => grid

--- js/snake.js
+++ js/snake.js
@@ -1,6 +1,6 @@
 
 let score = document.getElementById("score");
-let canvas = document.getElementById("canvas");
+let canvas = document.getElementById("grid");
 let snake = document.getElementById("snake");
 let apple = document.getElementById("apple");
 
@@ -12,7 +12,7 @@ function randapple() {
     let x = Math.floor(Math.random()*20)+1
     let y = Math.floor(Math.random()*20)+1
 
-    return document.getElementById("apple").style.gridArea= x+"/"+y+"/"+(x+1)+"/"+(y+1);
+    return document.getElementById("apple").style.gridArea= x+"/"+y;
 } 
 apple = randapple();
 
